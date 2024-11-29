d13a677d01679fb052a8247ebac06288f7b48c6d
anushaKankipati <anushakankipati1@gmail.com>
2024-11-28 22:39:23 -0800

    search by category

--- src/app/api/ads/route.ts
+++ src/app/api/ads/route.ts
@@ -1,9 +1,22 @@
 import { connect } from "@/src/libs/helpers";
-import { AdModel } from "@/src/models/Ad";
+import { Ad, AdModel } from "@/src/models/Ad";
+import { FilterQuery } from "mongoose";
 
 
-export async function GET(){
+export async function GET(req: Request, res: Response){
     await connect();
-    const adsDocs = await AdModel.find({}, null, {sort:{createdAt:-1}});
+    //console.log(req);
+    const {searchParams}= new URL(req.url);
+
+    const phrase = searchParams.get('phrase');
+    const category = searchParams.get('category');
+    const filter:FilterQuery<Ad> = {};
+    if(phrase){
+        filter.title = {$regex: '.*'+phrase+'.*', $options: 'i'};
+    }
+    if(category){
+        filter.category = category;
+    }
+    const adsDocs = await AdModel.find(filter, null, {sort:{createdAt:-1}});
     return Response.json(adsDocs);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,102 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Ad } from "../models/Ad";
-import UploadThumbnail from "../components/UploadThumbnail";
-import Link from "next/link";
+import AdItem from "../components/AdItem";
+import { categories } from "../libs/helpers";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStore } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   const [ads, setAds] = useState<Ad[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const formRef = useRef<HTMLFormElement | null>(null);
+
   useEffect(() => {
-    fetch('/api/ads').then(response => {
+    fetchAds();
+  }, []);
+
+  function fetchAds(params?: URLSearchParams) {
+    const url = `/api/ads?${params?.toString() || ''}`;
+    fetch(url).then(response => {
       response.json().then(adsDocs => {
         setAds(adsDocs);
       })
     });
-  }, []);
+  }
+
+  function handleSearch(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const params = new URLSearchParams();
+    formData.forEach((value, key) => {
+      if (typeof value === 'string') {
+        params.set(key, value);
+      }
+    });
+    fetchAds(params);
+  }
+
+  function handleCategoryChange(category: string) {
+    setSelectedCategory(category);
+    const formData = new FormData(formRef.current!);
+    formData.set('category', category);
+    const params = new URLSearchParams(formData as any);
+    fetchAds(params);
+  }
+
   return (
     <div className="flex w-full">
-      <div className="bg-gray-300 grow w-1/4">left</div>
-      <div className="p-4 grow w-3/4">
+      <form 
+        ref={formRef}
+        onSubmit={handleSearch}
+        className="bg-white grow w-1/4 p-4 border-r flex flex-col gap-4"
+      >
+        <input name="phrase" type="text" placeholder="Search SIDEQUE$T..." />
+        <div className="flex flex-col gap-0">
+          <label className={`radio-btn group ${selectedCategory === "" ? 'bg-blue-100' : ''}`}>
+            <input 
+              onClick={() => handleCategoryChange("")} 
+              className="hidden" 
+              type="radio" 
+              name="category" 
+              value="" 
+              checked={selectedCategory === ""}
+              onChange={() => {}}
+            />
+            <span className="icon group-has-[:checked]:bg-blue-500 group-has-[:checked]:text-white">
+              <FontAwesomeIcon icon={faStore}/>
+            </span>
+            All Categories
+          </label>
+          {categories.map(({key, label, icon}) => (
+            <label key={key} className={`radio-btn group ${selectedCategory === key ? 'bg-blue-100' : ''}`}>
+              <input 
+                onClick={() => handleCategoryChange(key)} 
+                className="hidden" 
+                type="radio" 
+                name="category" 
+                value={key}
+                checked={selectedCategory === key}
+                onChange={() => {}}
+              />
+              <span className="icon group-has-[:checked]:bg-blue-500 group-has-[:checked]:text-white">
+                <FontAwesomeIcon icon={icon}/>
+              </span>
+              {label}
+            </label>
+          ))}
+        </div>
+      </form>
+      <div className="p-4 grow bg-gray-100 w-3/4">
         <h2 className="font-bold mt-2 mb-4">Latest Tasks</h2>
         <div className="grid grid-cols-4 gap-x-4 gap-y-6">
           {ads.map(ad => (
-            <div key={ad._id} className="border-red-500 min-h-24 flex flex-col justify-start">
-              {ad.files?.length > 0 && (
-                <div className="rounded-md overflow overflow-hidden relative"> 
-                  <UploadThumbnail onClick={() =>{}} file={ad.files[0]} />
-                    <Link href={`/ad/${ad._id}`} className="absolute inset-0"></Link>
-                </div>
-              )}
-              <div>
-                <p className="mt-1 font-bold">${ad.price}</p>
-                <Link href={`/ad/${ad._id}`}>{ad.title}</Link>
-              </div>
-            </div>
+            <AdItem key={ad._id} ad={ad}/>
           ))}
         </div>
       </div>
     </div>
   );
 }
+
