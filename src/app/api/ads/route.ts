0b9af05919a14f5f23ba40635500441d996c67a8
LJablon <lucas.jablon@gmail.com>
2025-01-07 14:02:59 -0800

    Fixed numeric filters

--- src/app/api/ads/route.ts
+++ src/app/api/ads/route.ts
@@ -23,9 +23,9 @@ export async function GET(req: Request, res: Response) {
   if (category) {
     filter.category = category;
   }
-  if (min && !max) filter.price = { $gte: min };
-  if (max && !min) filter.price = { $lte: max };
-  if (min && max) filter.price = { $gte: min, $lte: max };
+  if (min && !max) filter.price = { $gte: parseInt(min) };
+  if (max && !min) filter.price = { $lte: parseInt(max) };
+  if (min && max) filter.price = { $gte: parseInt(min), $lte: parseInt(max) };
   if (radius && center) {
     const cords = center.split(",");
     aggregationSteps.push({
@@ -40,6 +40,7 @@ export async function GET(req: Request, res: Response) {
       },
     });
   }
+  console.log("filter", filter); 
   aggregationSteps.push({
     $match: filter,
   });
