45d05a9599bc854be937c765451b044272f6bb80
LJablon <lucas.jablon@gmail.com>
2025-01-07 13:31:12 -0800

    Added Login Guardrail for posting tasks

---
+++ src/app/new-profile/page.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function QuesterProfileSignupPage() {
+  return <h1>I am the QuesterProfileSignupPage!</h1>
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,52 +1,67 @@
-"use client";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Session } from "next-auth";
-import { signIn, signOut } from "next-auth/react";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+'use client'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Session } from 'next-auth'
+import { signIn, signOut } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
-export default function Header({ session }: { session: Session | null }) {
-  const router = useRouter();
-  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+export default function Header ({ session }: { session: Session | null }) {
+  const router = useRouter()
+  const [showDropdown, setShowDropdown] = useState<boolean>(false)
+
+  const handlePostClick = () => {
+    if (!session?.user) {
+      alert('You must Login or Signup before posting a task!')
+      return
+    }
+  }
+
   return (
-    <header className="border-b p-4 flex items-center justify-between h-16">
-      <Link className="text-blue-600 font-bold text-2xl" href="/">
+    <header className='border-b p-4 flex items-center justify-between h-16'>
+      <Link className='text-blue-600 font-bold text-2xl' href='/'>
         SIDEQUE$T
       </Link>
-      <nav className=" items-center flex gap-4 *:rounded ">
-        <Link
-          href="/new"
-          className="border h-full border-blue-600 text-blue-600 inline-flex gap-1 items-center px-2 mr-4 py-2"
-        >
-          <FontAwesomeIcon icon={faPlus} className="h-4" />
-          <span>Post a task</span>
-        </Link>
-        <span className="border-r"></span>
+      <nav className=' items-center flex gap-4 *:rounded '>
+        {session?.user ? (
+          <Link
+            href='/new'
+            className='border h-full border-blue-600 text-blue-600 inline-flex gap-1 items-center px-2 mr-4 py-2'
+          >
+            <FontAwesomeIcon icon={faPlus} className='h-4' />
+            <span>Post a task</span>
+          </Link>
+        ) : (
+          <div className='border h-full border-blue-600 text-blue-600 inline-flex gap-1 items-center px-2 mr-4 py-2'>
+            <FontAwesomeIcon icon={faPlus} className='h-4' />
+            <span onClick={handlePostClick}>Post a Quest</span>
+          </div>
+        )}
+        <span className='border-r'></span>
         {!session?.user && (
           <>
-            <button className="border-0 text-gray-600">Sign up</button>
+            <Link href="/new-profile" className='border-0 text-gray-600'>Sign up as a Quester</Link>
             <button
-              onClick={() => signIn("google")}
-              className="bg-blue-600 text-white border-0 px-6"
+              onClick={() => signIn('google')}
+              className='bg-blue-600 text-white border-0 px-6'
             >
-              Login
+              Login to Post Jobs
             </button>
           </>
         )}
         {session?.user && (
           <>
-            <div className="relative">
+            <div className='relative'>
               <button
-                onClick={() => setShowDropdown((prev) => !prev)}
-                title="profile-button"
+                onClick={() => setShowDropdown(prev => !prev)}
+                title='profile-button'
               >
                 <Image
-                  className={"rounded-md" + (showDropdown ? "z-50" : "")}
+                  className={'rounded-md' + (showDropdown ? 'z-50' : '')}
                   src={session.user.image as string}
-                  alt={"avatar"}
+                  alt={'avatar'}
                   width={36}
                   height={36}
                 />
@@ -55,20 +70,20 @@ export default function Header({ session }: { session: Session | null }) {
                 <>
                   <div
                     onClick={() => setShowDropdown(false)}
-                    className="bg-black/90 fixed inset-0 z-40"
+                    className='bg-black/90 fixed inset-0 z-40'
                   ></div>
-                  <div className="absolute z-50 right-0 top-8 bg-white rounded-md w-24 border">
+                  <div className='absolute z-50 right-0 top-8 bg-white rounded-md w-24 border'>
                     <button
-                      className="p-2 block text-center cursor-pointer w-full"
+                      className='p-2 block text-center cursor-pointer w-full'
                       onClick={() => {
-                        setShowDropdown(false);
-                        router.push("/my-ads");
+                        setShowDropdown(false)
+                        router.push('/my-ads')
                       }}
                     >
                       My Ads
                     </button>
                     <button
-                      className="p-2 block w-full cursor-pointer"
+                      className='p-2 block w-full cursor-pointer'
                       onClick={() => signOut()}
                     >
                       Logout
@@ -81,5 +96,5 @@ export default function Header({ session }: { session: Session | null }) {
         )}
       </nav>
     </header>
-  );
+  )
 }
