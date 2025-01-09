3782368e446dd2db8f17875f8a790d7cca21e5c3
LJablon <lucas.jablon@gmail.com>
2025-01-09 12:11:45 -0800

    Filter for hourly or Fixed rate works

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,74 +1,119 @@
-"use client";
+'use client'
 
-import { useEffect, useRef, useState } from "react";
-import { categories, defaultRadius } from "../../libs/helpers";
-import { faStore } from "@fortawesome/free-solid-svg-icons";
-import LabelRadioButton from "./LabelRadioButton";
-import SubmitButton from "./SubmitButton";
-import DistancePicker from "./DistancePicker";
-import { Location } from "./LocationPicker";
-import useCurrentLocation from "../hooks/useCurrentLocation";
-import useRadius from "../hooks/useRadius";
+import { useEffect, useRef, useState } from 'react'
+import { categories, defaultRadius } from '../../libs/helpers'
+import { faStore } from '@fortawesome/free-solid-svg-icons'
+import LabelRadioButton from './LabelRadioButton'
+import SubmitButton from './SubmitButton'
+import DistancePicker from './DistancePicker'
+import { Location } from './LocationPicker'
+import useCurrentLocation from '../hooks/useCurrentLocation'
+import useRadius from '../hooks/useRadius'
 
 type Props = {
-  onSearch: (formData: FormData) => void;
-};
+  onSearch: (formData: FormData) => void
+}
 
-export default function SearchForm({ onSearch }: Props) {
-  const radius = useRadius(state => state.radius); 
-  const setRadius = useRadius(state => state.setRadius); 
-  const center = useCurrentLocation((state) => state.currLocation);
-  const setCenter = useCurrentLocation((state) => state.setCurrLocation);
-  const [prevCenter, setPrevCenter] = useState<Location | null>(null);
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [minPrice, setMinPrice] = useState("");
-  const [maxPrice, setMaxPrice] = useState("");
-  const formRef = useRef<HTMLFormElement | null>(null);
+export default function SearchForm ({ onSearch }: Props) {
+  const radius = useRadius(state => state.radius)
+  const setRadius = useRadius(state => state.setRadius)
+  const center = useCurrentLocation(state => state.currLocation)
+  const setCenter = useCurrentLocation(state => state.setCurrLocation)
+  const [prevCenter, setPrevCenter] = useState<Location | null>(null)
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [minPrice, setMinPrice] = useState('')
+  const [maxPrice, setMaxPrice] = useState('')
+  const [isPayingHourly, setIsPayingHourly] = useState<boolean | undefined>()
+  const [fixedFilterClicked, setFixedFilterClicked] = useState<boolean>(false)
+  const [hourlyFilterClicked, setHourlyFilterClicked] = useState<boolean>(false)
+  const formRef = useRef<HTMLFormElement | null>(null)
   useEffect(() => {
     if (center && !prevCenter) {
-      formRef.current?.requestSubmit();
-      setPrevCenter(center);
+      formRef.current?.requestSubmit()
+      setPrevCenter(center)
     }
-  }, [center]);
+  }, [center])
 
-  function handleMinPriceChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setMinPrice(e.target.value);
+  function handleMinPriceChange (e: React.ChangeEvent<HTMLInputElement>) {
+    setMinPrice(e.target.value)
   }
 
-  function handleMaxPriceChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setMaxPrice(e.target.value);
+  function handleMaxPriceChange (e: React.ChangeEvent<HTMLInputElement>) {
+    setMaxPrice(e.target.value)
   }
 
-  function handleCategoryChange(category: string) {
-    setSelectedCategory(category);
-    const formData = new FormData(formRef.current!);
-    formData.set("category", category);
-    onSearch(formData);
+  function handleCategoryChange (category: string) {
+    setSelectedCategory(category)
+    const formData = new FormData(formRef.current!)
+    formData.set('category', category)
+    onSearch(formData)
   }
+
+  function handleOnlyHourly () {
+    // using this variable since react will update the state once the function exists and not in time 
+    // before the onSearch function executes
+    let searchState; 
+    if (!hourlyFilterClicked) {
+      searchState = true; 
+      setIsPayingHourly(true)
+      setFixedFilterClicked(false)
+      setHourlyFilterClicked(true)
+    } else {
+      searchState = undefined; 
+      setIsPayingHourly(undefined)
+      setHourlyFilterClicked(false); 
+    }
+    const formData = new FormData(formRef.current!)
+    formData.set('hourly', JSON.stringify(searchState))
+    console.log(formData.get('hourly'))
+    onSearch(formData)
+
+  }
+
+  function handleFixedRateOnly () {
+    let searchState; 
+    if (!fixedFilterClicked) {
+      searchState = false; 
+      setIsPayingHourly(false)
+      setFixedFilterClicked(true)
+      setHourlyFilterClicked(false)
+    } else {
+      searchState = undefined
+      setIsPayingHourly(undefined)
+      setFixedFilterClicked(false); 
+      console.log("here"); 
+    }
+    // setIsPayingHourly(false)
+    const formData = new FormData(formRef.current!)
+    formData.set('hourly', JSON.stringify(searchState))
+    console.log(formData)
+    onSearch(formData)
+  }
+
   useEffect(() => {
-    if (center) formRef.current?.requestSubmit();
-  }, []);
+    if (center) formRef.current?.requestSubmit()
+  }, [])
   return (
     <form
       ref={formRef}
       action={onSearch}
-      className="bg-white grow w-1/4 p-4 border-r flex flex-col gap-4"
+      className='bg-white grow w-1/4 p-4 border-r flex flex-col gap-4'
     >
-      <input name="phrase" type="text" placeholder="Search SIDEQUE$T..." />
-      <div className="flex flex-col gap-0">
+      <input name='phrase' type='text' placeholder='Search SIDEQUE$T...' />
+      <div className='flex flex-col gap-0'>
         <LabelRadioButton
-          key={"category"}
-          name={"category"}
-          value={""}
+          key={'category'}
+          name={'category'}
+          value={''}
           icon={faStore}
           onClick={handleCategoryChange}
-          isSelected={selectedCategory === ""}
-          label={"All Categories"}
+          isSelected={selectedCategory === ''}
+          label={'All Categories'}
         />
         {categories.map(({ key: categoryKey, label, icon }) => (
           <LabelRadioButton
             key={categoryKey}
-            name={"category"}
+            name={'category'}
             value={categoryKey}
             icon={icon}
             onClick={handleCategoryChange}
@@ -77,23 +122,53 @@ export default function SearchForm({ onSearch }: Props) {
           />
         ))}
       </div>
-      <div className="">
+      <div className='flex justify-between items-center flex-wrap'>
+        <div>
+          <input
+            className={
+              (hourlyFilterClicked
+                ? 'bg-blue-600 text-white'
+                : 'bg-white text-gray-400 border border-gray-400') +
+              ' mt-2 px-6 py-2 rounded cursor-pointer'
+            }
+            type='button'
+            name='hourly'
+            value={'Hourly Only'}
+            onClick={handleOnlyHourly}
+          />
+        </div>
+        <div>
+          <input
+            className={
+              (fixedFilterClicked
+                ? 'bg-blue-600 text-white'
+                : 'bg-white text-gray-400 border border-gray-400') +
+              ' mt-2 px-6 py-2 rounded cursor-pointer'
+            }
+            type='button'
+            name="hourly"
+            value={'Fixed Only'}
+            onClick={handleFixedRateOnly}
+          />
+        </div>
+      </div>
+      <div className=''>
         <label>Filter by price</label>
-        <div className="grid grid-cols-2 gap-4">
+        <div className='grid grid-cols-2 gap-4'>
           <div>
             <input
-              name="min"
-              type="number"
-              placeholder="min"
+              name='min'
+              type='number'
+              placeholder='min'
               value={minPrice}
               onChange={handleMinPriceChange}
             />
           </div>
           <div>
             <input
-              name="max"
-              type="number"
-              placeholder="max"
+              name='max'
+              type='number'
+              placeholder='max'
               value={maxPrice}
               onChange={handleMaxPriceChange}
             />
@@ -101,21 +176,21 @@ export default function SearchForm({ onSearch }: Props) {
         </div>
       </div>
       <div>
-        <input type="hidden" name="radius" value={radius}></input>
+        <input type='hidden' name='radius' value={radius}></input>
         <input
-          type="hidden"
-          name="center"
+          type='hidden'
+          name='center'
           value={[JSON.stringify(center?.lat), JSON.stringify(center?.lng)]}
         ></input>
         <DistancePicker
           defaultRadius={defaultRadius}
           onChange={({ radius, center }) => {
-            setRadius(radius);
-            setCenter(center);
+            setRadius(radius)
+            setCenter(center)
           }}
         />
       </div>
       <SubmitButton>Search</SubmitButton>
     </form>
-  );
+  )
 }
