4c65c1e4b61c02ec2fa8e875d9ece092469e1b67
anushaKankipati <anushakankipati1@gmail.com>
2025-01-13 23:00:30 -0800

    home page and ad page UI changed to wireframe

--- src/components/AdItem.tsx
+++ src/components/AdItem.tsx
@@ -7,17 +7,29 @@ import { formatMoney } from "@/libs/helpers";
 
 export default function AdItem({ad}:{ad:Ad}){
     return (
-        <Link href={`/ad/${ad._id}`} className="p-4 bg-gray-100 rounded-md min-h-24 flex flex-col justify-start">
+        <Link href={`/ad/${ad._id}`} className="p-4 bg-gray-100 rounded-2xl min-h-24 flex flex-col justify-start">
               {ad.files?.length > 0 && (
                 <div className="rounded-md overflow overflow-hidden relative"> 
-                  <UploadThumbnail onClick={() =>{}} file={ad.files[0]} />
                     <Link href={`/ad/${ad._id}`} className="absolute inset-0"></Link>
                 </div>
               )}
-              <div>
+              <div className="p-2">
                 <div className="mt-1 text-2xl">{ad.title}</div>
                 <p className="">{ad.isPayingByHour ? "Hourly Rate: " : "Quest Bounty: "}<span className="font-bold">{ad.isPayingByHour ? formatMoney(ad.price) + "/hr" : formatMoney(ad.price)}</span></p>
-              </div>
+                <p className="mt-3 mb-3">{ad.description }</p>
+                <label>Quest Tags</label>
+                {ad?.tags.length > 0 ? (
+                  <div className="flex flex-wrap">
+                    {ad.tags.map((tag, index) => (
+                      <span key={index} className="text-sm tag mr-1 mt-1 mb-1">
+                        {tag}
+                      </span>
+                    ))}
+                  </div>
+                ) : (
+                  <p className="text-sm">No Tags Found for this Quest</p>
+                )}
+                      </div>
         </Link>
     );
 }
