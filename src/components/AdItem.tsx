34982e0b14e780c88c8090608bd1366dfa08d69e
LJablon <lucas.jablon@gmail.com>
2025-01-22 18:23:02 -0800

    UI changes to match HomeAdsViewWireframe

--- src/components/AdItem.tsx
+++ src/components/AdItem.tsx
@@ -1,30 +1,60 @@
-"use client";
+'use client'
 
-import Link from "next/link";
-import { Ad } from "../models/Ad";
-import { formatLongText, formatMoney } from "@/libs/helpers";
+import Link from 'next/link'
+import { Ad } from '../models/Ad'
+import { formatLongText, formatMoney } from '@/libs/helpers'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import FavoriteButton from './FavoriteButton'
 
-export default function AdItem({ad}:{ad:Ad}){
-    return (
-        <Link href={`/ad/${ad._id}`} className="p-4 bg-gray-100 rounded-2xl min-h-24 flex flex-col justify-start">
-              <div className="p-2">
-                <div className="mt-1 text-2xl">{ad.title}</div>
-                {ad?.cityName && <div>{ad?.cityName} {"- _ miles from you"}</div>}
-                <p className="">{ad.isPayingByHour ? "Hourly Rate: " : "Fixed Rate: "}<span className="font-bold">{ad.isPayingByHour ? formatMoney(ad.price) + "/hr" : formatMoney(ad.price)}</span></p>
-                <p className="mt-3 mb-3">{ad.description.length > 400 ?  formatLongText(ad.description) : ad.description }</p>
-                <label>Quest Tags</label>
-                {ad?.tags.length > 0 ? (
-                  <div className="flex flex-wrap">
-                    {ad.tags.map((tag, index) => (
-                      <span key={index} className="text-sm tag mr-1 mt-1 mb-1">
-                        {tag}
-                      </span>
-                    ))}
-                  </div>
-                ) : (
-                  <p className="text-sm">No Tags Found for this Quest</p>
-                )}
-                      </div>
-        </Link>
-    );
+export default function AdItem ({ ad }: { ad: Ad }) {
+  return (
+    <Link
+      href={`/ad/${ad._id}`}
+      className='p-4 bg-gray-100 rounded-2xl min-h-24 flex flex-col justify-start'
+    >
+      <div className='p-2'>
+        <div className='flex justify-between align-top'>
+          <div className='flex flex-col'>
+            <h1 className='text-2xl'>{ad.title}</h1>
+            {ad?.cityName && (
+              <div>
+                {ad?.cityName} {'- _ miles from you'}
+              </div>
+            )}
+          </div>
+          <div className='flex align-top'>
+            <div className='flex flex-col mr-6 align-bottom'>
+              <p className=''>
+                {ad.isPayingByHour ? 'Hourly Rate ' : 'Fixed Rate '}
+              </p>
+              <span className='font-bold ml-auto'>
+                {ad.isPayingByHour
+                  ? formatMoney(ad.price) + '/hr'
+                  : formatMoney(ad.price)}
+              </span>
+            </div>
+            <FavoriteButton/>
+          </div>
+        </div>
+        <p className='mt-3 mb-3'>
+          {ad.description.length > 400
+            ? formatLongText(ad.description)
+            : ad.description}
+        </p>
+        <label>Quest Tags</label>
+        {ad?.tags.length > 0 ? (
+          <div className='flex flex-wrap'>
+            {ad.tags.map((tag, index) => (
+              <span key={index} className='text-sm tag mr-1 mt-1 mb-1'>
+                {tag}
+              </span>
+            ))}
+          </div>
+        ) : (
+          <p className='text-sm'>No Tags Found for this Quest</p>
+        )}
+      </div>
+    </Link>
+  )
 }
---
+++ src/components/FavoriteButton.tsx
@@ -0,0 +1,11 @@
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function FavoriteButton() {
+  return (
+    <FontAwesomeIcon
+      icon={faHeart}
+      className="fa-xl p-1.5 bg-gray-200 rounded-[50%] hover:bg-light-hover-green hover:color-green"
+    />
+  );
+}
