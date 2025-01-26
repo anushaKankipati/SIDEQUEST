91da7eef13e4dfc02c3e7534784901d3b66d1b12
LJablon <lucas.jablon@gmail.com>
2025-01-25 18:54:38 -0800

    Fixed the Loading Display bug for the AutocompleteMap.tsx component

--- src/components/AutoCompleteMap.tsx
+++ src/components/AutoCompleteMap.tsx
@@ -6,15 +6,18 @@ import useCurrentLocation from "../hooks/useCurrentLocation";
 import { FormattedAutocompleteLocation } from "@/libs/types";
 import { Location } from "./LocationPicker";
 
-
 interface AutoCompleteMapProps {
-  mapHeight?: string; 
+  mapHeight?: string;
   onLocationChange: (location: Location) => void;
-  onFormattedLocationChange: (location: FormattedAutocompleteLocation) => void;  
+  onFormattedLocationChange: (location: FormattedAutocompleteLocation) => void;
 }
 
 const libs: Library[] = ["core", "maps", "marker", "places"];
-export default function AutoCompleteMap({ mapHeight, onLocationChange, onFormattedLocationChange }: AutoCompleteMapProps) {
+export default function AutoCompleteMap({
+  mapHeight,
+  onLocationChange,
+  onFormattedLocationChange,
+}: AutoCompleteMapProps) {
   const [map, setMap] = useState<google.maps.Map | null>(null);
   const [autoComplete, setAutoComplete] =
     useState<google.maps.places.Autocomplete>();
@@ -28,9 +31,7 @@ export default function AutoCompleteMap({ mapHeight, onLocationChange, onFormatt
   const setCurrentLocation = useCurrentLocation(
     (state) => state.setCurrLocation
   );
-  const [place, setPlace] = useState<string | null>(null); 
-
-
+  const [place, setPlace] = useState<string | null>(null);
 
   useEffect(() => {
     if (isLoaded && google && google.maps && google.maps.places) {
@@ -38,33 +39,42 @@ export default function AutoCompleteMap({ mapHeight, onLocationChange, onFormatt
         center: currentLocation,
         zoom: 17,
         id: "google-map-script",
-        mapId: "autocomplete-map", 
+        mapId: "autocomplete-map",
       };
-      const gMap = new google.maps.Map(mapRef.current as HTMLDivElement, mapOptions); 
-    
-      //setup autocomplete 
+      const gMap = new google.maps.Map(
+        mapRef.current as HTMLDivElement,
+        mapOptions
+      );
+
+      //setup autocomplete
       const autoCompleteOptions = {
-        fields: ["formatted_address", "name", "geometry", "vicinity", "place_id"], 
-      }
-      const googleAutoComplete = new google.maps.places.Autocomplete(placesAutoCompleteRef.current as HTMLInputElement, autoCompleteOptions);
+        fields: [
+          "formatted_address",
+          "name",
+          "geometry",
+          "vicinity",
+          "place_id",
+        ],
+      };
+      const googleAutoComplete = new google.maps.places.Autocomplete(
+        placesAutoCompleteRef.current as HTMLInputElement,
+        autoCompleteOptions
+      );
       setAutoComplete(googleAutoComplete);
       setMap(gMap);
-
     }
   }, [isLoaded]);
 
   function setMarker(location: google.maps.LatLng, name: string) {
     if (!map) {
-      return; 
+      return;
     }
-    map.setCenter(location); 
-    const marker = new google.maps.marker.AdvancedMarkerElement(
-      {
-        map: map, 
-        position: location, 
-        title: "Marker"
-      }
-    )
+    map.setCenter(location);
+    const marker = new google.maps.marker.AdvancedMarkerElement({
+      map: map,
+      position: location,
+      title: "Marker",
+    });
   }
 
   // Infowindow library for info pop ups
@@ -72,46 +82,50 @@ export default function AutoCompleteMap({ mapHeight, onLocationChange, onFormatt
   useEffect(() => {
     if (autoComplete) {
       autoComplete.addListener("place_changed", () => {
-        const place = autoComplete.getPlace(); 
+        const place = autoComplete.getPlace();
         onFormattedLocationChange({
-          formatted_address: place.formatted_address as string, 
-          name: place.name as string, 
+          formatted_address: place.formatted_address as string,
+          name: place.name as string,
           location: {
-            lng: place.geometry?.location?.lng() as number, 
+            lng: place.geometry?.location?.lng() as number,
             lat: place.geometry?.location?.lat() as number,
           },
-          vicinity: place.vicinity as string, 
-          place_id: place.place_id as string, 
-        })
+          vicinity: place.vicinity as string,
+          place_id: place.place_id as string,
+        });
         onLocationChange({
-          lng: place.geometry?.location?.lng() as number, 
+          lng: place.geometry?.location?.lng() as number,
           lat: place.geometry?.location?.lat() as number,
-        })
-        setPlace(place.formatted_address as string); 
-        const position = place.geometry?.location; 
+        });
+        setPlace(place.formatted_address as string);
+        const position = place.geometry?.location;
         if (position) {
-          setMarker(position, place.name!); 
+          setMarker(position, place.name!);
         } // TODO: quirk to work out later: let the default location be displayed before the autocomplete
         // else {
         //   const currentPosition = new google.maps.LatLng({
-        //     lat: currentLocation?.lat as number, 
+        //     lat: currentLocation?.lat as number,
         //     lng: currentLocation?.lng as number,
         //   });
         //   setMarker(currentPosition, "");
         // }
-      })
-  }
-  }, [autoComplete])
+      });
+    }
+  }, [autoComplete]);
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  } 
 
   return (
     <div className="flex flex-col space-y-4">
-      <input type="text" placeholder="Quest Location" ref={placesAutoCompleteRef}/>
+      <input
+        type="text"
+        placeholder="Quest Location"
+        ref={placesAutoCompleteRef}
+      />
       <label>{place}</label>
-      {isLoaded ? (
-        <div style={{ height: mapHeight}} ref={mapRef} />
-      ) : (
-        <p>Loading...</p>
-      )}
+
+      <div style={{ height: mapHeight }} ref={mapRef} />
     </div>
   );
 }
