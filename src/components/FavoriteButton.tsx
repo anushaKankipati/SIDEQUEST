89d52c178ef53773cdbae84c26bf9ea95f38eec9
LJablon <lucas.jablon@gmail.com>
2025-02-03 20:57:04 -0800

    slight color change

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -5,7 +5,7 @@ export default function FavoriteButton() {
   return (
     <FontAwesomeIcon
       icon={faHeart}
-      className="fa-xl p-1.5 bg-gray-200 rounded-[50%] hover:bg-green-400 hover:color-green"
+      className="fa-xl p-1.5 bg-gray-200 rounded-[50%] hover:bg-green-300 hover:color-green"
     />
   );
 }
