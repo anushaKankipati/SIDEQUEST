7eeb6015879d3b6c4bd1f67d3d5524b202375963
LJablon <lucas.jablon@gmail.com>
2025-04-21 20:01:09 -0700

    updated [.../nextauth] - route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,67 @@
-import NextAuth from "next-auth";
+import NextAuth, {AuthOptions, SessionStrategy} from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import bcrypt from "bcrypt";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/libs/prismadb";
+import { adapter } from "next/dist/server/web/adapter";
+import { PassThrough } from "stream";
+import { SessionState } from "http2";
 
 export const authOptions = {
-    secret: process.env.SECRET,
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        }),
-        
-    ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      // have to go through the formal registration page with name email and password
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid Credentials");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user?.hashedPassword) {
+          throw new Error(
+            "Invalid Credentials - user not found user not registered without a provider"
+          );
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid Credentials - Incorrect Password");
+        }
+
+        return user; 
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt" as SessionStrategy,
+  }, 
 };
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
