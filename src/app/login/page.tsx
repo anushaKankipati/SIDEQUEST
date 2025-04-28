8d5dc0443b4d60ed5950ea0725b24e59ddf446a1
LJablon <lucas.jablon@gmail.com>
2025-04-28 15:04:49 -0700

    test for short and tall breakpoints

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,9 @@
 import AuthForm from "@/src/components/AuthForm";
 import LoginLogo from "@/src/components/LoginLogo";
-import Image from "next/image";
 
 export default async function Login() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white px-4 mt-14 2xl:mt-0">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white px-4 short:mt-14 tall:mt-0">
       <LoginLogo/>
       <AuthForm />
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,25 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/**/*.{html,js,tsx}"
-  ],
+  content: ["./src/**/*.{html,js,tsx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        "theme-green": "var(--theme-green)", 
+        "theme-green": "var(--theme-green)",
         "theme-black": "var(--theme-black)",
         "light-hover-green": "var(--light-hover-green)",
+      },
+      screens: {
+        tall: { raw: "(min-height: 826px)" },
+        short: { raw: "(max-height: 825px)" },
       },
     },
   },
   plugins: [
     require("@tailwindcss/forms")({
-      strategy: 'class',
-    })
+      strategy: "class",
+    }),
   ],
 } satisfies Config;
