cadcf7fd948a4bdec5b1c5a3553e10b0e49489e7
LJablon <lucas.jablon@gmail.com>
2025-04-29 19:26:25 -0700

    User page complete

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,12 +1,18 @@
 import Sidebar from "@/src/components/sidebar/Sidebar";
+import getUsers from "../actions/getUsers";
+import UserList from "./components/UserList";
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
   return(
   <Sidebar>
-    <div className="h-screen">{children}</div>;
+    <div className="h-screen">
+      <UserList items={users}/>
+      {children}
+    </div>;
   </Sidebar>);
 }
