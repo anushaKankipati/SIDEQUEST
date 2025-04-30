4e6eb63d72a3b70af24c6ea30463b2d5e9803ccf
LJablon <lucas.jablon@gmail.com>
2025-04-29 18:46:54 -0700

    users and messaging basic layout

---
+++ src/app/users/layout.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "@/src/components/sidebar/Sidebar";
+
+export default async function UsersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return(
+  <Sidebar>
+    <div className="h-screen">{children}</div>;
+  </Sidebar>);
+}
---
+++ src/components/sidebar/MobileFooter.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export default function MobileFooter() {
+  return <div>
+    Mobile Footer
+  </div>
+}
